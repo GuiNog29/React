523a7da196e5b049fdbf71c878696b3d3a6a66f0
Guilherme Almeida <guimathiasgui@gmail.com>
2022-05-02 21:12:50 -0300

    Eslint and Editorconfig

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import './App.css'
+import React from 'react';
+import './App.css';
 
 export default function App() {
-  return <h1> Heloo World </h1>
+  return <h1> Heloo World </h1>;
 }
